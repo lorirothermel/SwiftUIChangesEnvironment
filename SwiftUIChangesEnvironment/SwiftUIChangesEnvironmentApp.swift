2d047364e3a2b6222f635b5d7aeea5fdeb718c7b
Lori Rothermel <lori@rothermel1.com>
2024-12-12 18:20:11 -0800

    Initial Commit

---
+++ SwiftUIChangesEnvironment/SwiftUIChangesEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChangesEnvironmentApp.swift
+//  SwiftUIChangesEnvironment
+//
+//  Created by Lori Rothermel on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChangesEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
